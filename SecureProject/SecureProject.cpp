291d1aee25df01caf16ad666161a5532e254b38a
PeLaTuni <peetu.lahtinen@tuni.fi>
2024-05-10 17:34:24 +0300

    Final changes

--- SecureProject/SecureProject.cpp
+++ SecureProject/SecureProject.cpp
@@ -1,26 +1,18 @@
 /* Secure Project
+- Made by Peetu Lahtinen
 - Testing password strength and solving hashed passwords
-- https://github.com/danielmiessler/SecLists/tree/master/Passwords
+- I got the wordlist from here: https://github.com/danielmiessler/SecLists/tree/master/Passwords
 */
 
 
 
 // Includes
 #include "Cli.h"
-#include "Meta.h"
 
 
 
 // Main
 int main() {
-	// Muutokset (poista)
-	Meta meta;
-	//std::vector<std::string> param = { "3A5E8D2B71F956C4B90A2D38FEB4C7E9" };
-	//meta.set_salt(param);
-	//meta.create_rainbowtable();
-	//meta.create_hashfile();
-	//meta.create_big_hashfile();
-
 	Cli cli;
 
 	// Loop
